12a2d6ef1fe70029962ab875d431eb1cd3ed812e
Izhar Sanz <izhar.sanz@zataca.com>
2020-01-03 02:39:09 +0100

    quitando clousure

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('{path}', function () {
-    return view('index');
-})->where('path', '(.*)');
+// Route::get('{path}', function () {
+//     return view('index');
+// })->where('path', '(.*)');
